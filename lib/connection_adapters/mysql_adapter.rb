bec6d08a614d21e9949c6e05c0a598ee359839be
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2010-02-22 18:12:27 +0100

    Pushed valid mysql_adapter

--- lib/connection_adapters/mysql_adapter.rb
+++ lib/connection_adapters/mysql_adapter.rb
@@ -1,7 +1,24 @@
 module ActiveRecord
-  module ConnectionAdapter
+  module ConnectionAdapters
     class MysqlAdapter < AbstractAdapter
-      NATIVE_DATABASE_TYPES[:primary_key] = "varchar(36) DEFAULT NULL PRIMARY KEY"
+      def native_database_types
+        {
+          #        :primary_key => "int(11) DEFAULT NULL auto_increment PRIMARY KEY".freeze,
+          :primary_key => "varchar(36) PRIMARY KEY".freeze,
+          :string      => { :name => "varchar", :limit => 255 },
+          :text        => { :name => "text" },
+          :integer     => { :name => "int", :limit => 4 },
+          :float       => { :name => "float" },
+          :decimal     => { :name => "decimal" },
+          :datetime    => { :name => "datetime" },
+          :timestamp   => { :name => "datetime" },
+          :time        => { :name => "time" },
+          :date        => { :name => "date" },
+          :binary      => { :name => "blob" },
+          :boolean     => { :name => "tinyint", :limit => 1 }
+        }
+      end
+
     end
   end
 end
