665a132c16eba2461f887bf2a876021b7835887f
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2010-02-22 18:07:45 +0100

    Deleted invalid mysql_adapter

--- lib/connection_adapters/mysql_adapter.rb
+++
@@ -1,7 +0,0 @@
-module ActiveRecord
-  module ConnectionAdapter
-    class MysqlAdapter < AbstractAdapter
-      NATIVE_DATABASE_TYPES[:primary_key] = "varchar(36) DEFAULT NULL PRIMARY KEY"
-    end
-  end
-end
